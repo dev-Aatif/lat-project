5d8e267cf8978d979381d3fb9e936a918db4e59a
dev-Aatif <Aatiff.P@proton.me>
2025-09-13 19:01:03 +0500

    1.8.2

--- src/components/Questioncard.tsx
+++ src/components/Questioncard.tsx
@@ -20,7 +20,7 @@ type Props = {
 }
 
 // Fisher-Yates shuffle algorithm
-const shuffleArray = (array: any[]) => {
+const shuffleArray = <T,>(array: T[]): T[] => {
   const shuffled = [...array]
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
